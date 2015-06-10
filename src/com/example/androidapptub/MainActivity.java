1690ebb7e3b5ef0418147847a32938495793d952
Jimmy Visse <jimmy.visse@gmail.com>
2015-06-10 23:43:28 +0200

    Implémentation de l'interaction entre plusieurs activité

--- src/com/example/androidapptub/MainActivity.java
+++ src/com/example/androidapptub/MainActivity.java
@@ -1,15 +1,63 @@
 package com.example.androidapptub;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SensorEventListener {
 
+	private SensorManager mSensorManager;
+	private Sensor mAccelerometer;
+	private Button button_place = null;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        
+        // Get place button and affect callback on this button
+        button_place = (Button) findViewById(R.id.button_places);
+        button_place.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent placeAct = new Intent(MainActivity.this, ActivityPlaces.class);
+				startActivity(placeAct);
+			}
+		});
     }
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	protected void onResume() {
+		super.onResume();
+	}
+	
+	protected void onPause() {
+		super.onPause();
+		mSensorManager.unregisterListener(this);
+	}
 }
